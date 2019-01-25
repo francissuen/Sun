4bf48eda6c8cab49f8c2eb31e138f60b51a0d1fa
Francis Sun <francisjsun@outlook.com>
2019-01-25 17:54:17 +0800

    FS_SUN_VERBOSE is supporting function parameters now

--- src/file.h
+++ src/file.h
@@ -8,21 +8,22 @@
 
 FS_SUN_NS_BEGIN
 
-	FS_SUN_CLASS file
-	{
-	public:
-	    file(const char* filePath, bool bRead = true);
-	    ~file();
-	    void read(void* const buffer, const size_t bufferSize);
-	    void write(const void* data, const size_t size);
-	    void close();
+FS_SUN_CLASS file
+{
+public:
+file(const char* filePath, bool bRead = true);
+~file();
+bool read(void* const buffer, const size_t bufferSize);
+bool write(const void* data, const size_t size);
+void close();
 	    
-	    bool eof()const { return _file.eof(); }
-	    std::uint32_t size() const { return _size; }
-	private:
-	    std::fstream _file;
-	    std::uint32_t _size;
-	    string _filePath;
-	};
+inline bool eof()const { return _file.eof(); }
+inline bool is_open() const { return _file.is_open(); }
+std::uint32_t size() const { return _size; }
+private:
+std::fstream _file;
+std::uint32_t _size;
+string _filePath;
+};
 
 FS_SUN_NS_END
