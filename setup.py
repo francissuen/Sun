739dc1a14e01c651e2bbf1908d4b358b1794f7d3
Francis Sun <francisjsun@outlook.com>
2019-04-03 13:41:06 +0800

    rm submodule fsCMake

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
 from mars import dependency
 
+fsCMake = {
+    "name": "fsCMake",
+    "ver": "v0.0.1",
+    "addr": """\
+https://github.com/francissuen/fsCMake/releases/download/{0}/fsCMake.tar.xz"""}
+
+fsCMake["addr"] = fsCMake["addr"].format(fsCMake["ver"])
+
 dependencies = [
     dependency.Dependency({
-        "name": "fsCMake",
-        "addr": """https://github.com/francissuen/fsCMake/releases/download/\
-        v1.0.3\
-        /fsCMake.tar.xz"""})
-]
+        "name": fsCMake["name"],
+        "addr": fsCMake["addr"]})]
 
 for d in dependencies:
     d.fix()
