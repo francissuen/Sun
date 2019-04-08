eff3699a323f61c4b80a4db31d4bdfd4c77cccce
Francis Sun <francisjsun@outlook.com>
2019-04-08 18:46:36 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -7,9 +7,6 @@ except ImportError:
                     "git+https://github.com/francissuen/mars.git@master"])
     from mars import dependency
 
-import os
-import shutil
-
 fsCMake = {
     "name": "fsCMake",
     "ver": "v0.0.0",
@@ -18,15 +15,8 @@ https://github.com/francissuen/fsCMake/releases/download/{0}/fsCMake.tar.xz"""}
 
 fsCMake["addr"] = fsCMake["addr"].format(fsCMake["ver"])
 
-dependencies = [
-    dependency.Dependency({
-        "name": fsCMake["name"],
-        "addr": fsCMake["addr"]})]
+deps = dependency.Dependency()
 
-old_cwd = os.getcwd()
+deps.add(dependency.DepInfo(fsCMake))
 
-for d in dependencies:
-    d.fix()
-
-os.chdir(old_cwd)
-shutil.copy("fsCMake/build.sh", "./")
+deps.fix()
