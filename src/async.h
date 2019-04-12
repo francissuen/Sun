3a6f4871b75b8bc5d0355e69f1f7e6fb0143b5b5
Francis Sun <francisjsun@outlook.com>
2019-04-12 18:16:10 +0800

    add async

---
+++ src/async.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2019, F.S.. All rights reserved.
+ */
+
+#pragma once
+
+#include "ns.h"
+
+FS_SUN_NS_BEGIN
+
+async.do([=]()
+        {
+            real_job(...);
+        })
+
+FS_SUN_NS_END
